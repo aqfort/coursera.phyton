89afd5f76a6b96577114bf038db3c1fa2393a222
aqfort <valiev1379@gmail.com>
2020-09-30 15:33:51 +0300

    some tasks

---
+++ w3/w3_1.py
@@ -0,0 +1,23 @@
+class FileReader:
+    """
+    FileReader class
+    """
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def __str__(self):
+        return f"source: {self.file_path}"
+    
+    def __repr__(self):
+        return f"source: {self.file_path}"
+
+    def read(self):
+        try:
+            with open(self.file_path, "r") as f:
+                return f.read()
+        except FileNotFoundError:
+            return ""
+
+
+if __name__ == "__main__":
+    pass
---
+++ w3/w3_2.py
@@ -0,0 +1,31 @@
+
+
+
+class CarBase:
+    """
+    CarBase class
+    """
+    pass
+
+class Car(CarBase):
+    """
+    Car class
+    """
+    pass
+
+class Truck(CarBase):
+    """
+    Truck class
+    """
+    pass
+
+class SpecMachine(CarBase):
+    """
+    SpecMachine class
+    """
+    pass
+
+
+
+if __name__ == "__main__":
+    pass
